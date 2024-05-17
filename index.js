5752b6c9d750aa855dea716b4462c042bdb425e0
qdwtfuh <gs1harrocho@hotmail.com>
2024-05-17 12:57:02 +0700

    test task

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function getMax(arr) {
-  return Math.max(...arr);
+function maxSlidingWindow(nums, k) {
+  const result = [];
+  const queue = [];
+  for (let i = 0; i < nums.length; i++) {
+    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
+      queue.pop();
+    }
+    queue.push(i);
+    if (queue[0] === i - k) queue.shift();
+    if (i >= k - 1) result.push(nums[queue[0]]);
+  }
+  return result;
 }
